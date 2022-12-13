2bf7e6938a2b9b7525299dd2935a569f8486e18e
CodexisPhantom <85877781+CodexisPhantom@users.noreply.github.com>
2022-12-13 11:21:00 +0100

    Update ScoreManager.cs

--- Assets/Scripts/EndingScreen/ScoreManager.cs
+++ Assets/Scripts/EndingScreen/ScoreManager.cs
@@ -5,9 +5,13 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
+    private int score = 0;
     // Start is called before the first frame update
     void Start()
     {
+        if (StaticGameData.Game.Points > score) {
+            this.score = StaticGameData.Game.Points;
+        }
         this.GetComponent<Text>().text = "" + StaticGameData.Game.Points;
     }
 }
